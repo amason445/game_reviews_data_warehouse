debbe24e6787cdf9aeaaf36ab9ba1490d54af325
Alex Mason <77219320+amason445@users.noreply.github.com>
2023-09-09 14:46:50 -0600

    Update StagingTablesDDL.sql

--- SQL - DDL/Tables/StagingTablesDDL.sql
+++ SQL - DDL/Tables/StagingTablesDDL.sql
@@ -11,14 +11,29 @@ CREATE TABLE STAGE.fact_GameReviews (
 	RawgRatingsCount integer,
 	MetacriticScore integer);
 
-CREATE TABLE STAGE.dim_DeveloperTable (
-	DeveloperId integer NOT NULL PRIMARY KEY,
-	DeveloperName varchar,
-	DeveloperCount integer);
-
 CREATE TABLE STAGE.dim_date (
 	Date date NOT NULL PRIMARY KEY,
 	year integer,
 	quarter integer,
 	month integer,
 	day integer);
+
+CREATE TABLE STAGE.dim_DeveloperTable (
+	DeveloperId integer NOT NULL PRIMARY KEY,
+	DeveloperName varchar,
+	DeveloperCount integer);
+
+CREATE TABLE STAGE.dim_GenreTable (
+	GenreId integer NOT NULL PRIMARY KEY,
+	GenreName varchar,
+	GenreCount integer);
+
+CREATE TABLE STAGE.dim_GenreBridgeTable (
+	GenreGameKey varchar NOT NULL PRIMARY KEY,
+	GenreId integer,
+	GameId integer);
+
+CREATE TABLE STAGE.dim_PlatformTable (
+	PlatformId integer NOT NULL PRIMARY KEY,
+	PlatformName varchar,
+	PlatformCount integer);
