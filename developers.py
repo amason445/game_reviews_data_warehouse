926f3c8054a1e87b71ea0db32212f3e5e132b406
Alex Mason <77219320+amason445@users.noreply.github.com>
2023-09-09 21:24:27 -0600

    Updated Developers Scrape

--- developers.py
+++ developers.py
@@ -3,61 +3,85 @@ from datetime import datetime
 import toml
 import requests
 import utilities
+import pyodbc
 
 logging.basicConfig(filename='logging\\developers.log', encoding='utf-8', level=logging.INFO, 
                     format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
-def initial_scrape(scrape_timestamp):
+config = toml.load("config.toml")
 
-    #variables store results space and logging information
-    results_list = []
-    game_count = 0
+# def initial_scrape():
 
-    #sets API request limit for pagination feature
-    page_limit = 10
+#     #variables store results space and logging information
+#     results_list = []
+#     game_count = 0
 
-    config = toml.load("config.toml")
-    URL = f"https://api.rawg.io/api/developers?key={config['APIkeys']['rawgio_key']}&page_size=50"
+#     #sets API request limit for pagination feature
+#     page_limit = 10
 
-    try:
-        developers_request = requests.get(URL).json()
+
+#     URL = f"https://api.rawg.io/api/developers?key={config['APIkeys']['rawgio_key']}&page_size=50"
+
+#     try:
+#         developers_request = requests.get(URL).json()
         
-        for i in range(0, len(developers_request['results'])):
-            results_list.append(developers_request['results'][i])
-            game_count += developers_request['results'][i]['games_count']
+#         for i in range(0, len(developers_request['results'])):
+#             results_list.append(developers_request['results'][i])
+#             game_count += developers_request['results'][i]['games_count']
 
-        next_page = developers_request['next']
+#         next_page = developers_request['next']
     
-    except Exception as e:
-        logging.error(f"Initial request failed:\n{e}")
+#     except Exception as e:
+#         logging.error(f"Initial request failed:\n{e}")
 
-    for i in range(0, page_limit):
+#     for i in range(0, page_limit):
 
+#         try:
+#             developers_request = requests.get(next_page).json()
+
+#             for i in range(0, len(developers_request['results'])):
+#                 results_list.append(developers_request['results'][i])
+#                 game_count += developers_request['results'][i]['games_count']
+
+#             logging.info(f"URL {next_page} was successful")
+
+#             next_page = developers_request['next']
+
+#         except Exception as e:
+#             logging.error(f"Request failed:\n{e}")
+
+
+#     logging.info(f"Initial developer request was successful.\nCount of developers: {len(results_list)}\nCount of scrapeable games: {game_count}")
+        
+    
+    #write intial extract to temporary JSON file using utilities.py
+    #utilities.write_to_json(results_list, config['JSONarchive']['developer_extract'])
+
+def transform_to_load_set(scrape_timestamp: datetime):
+
+    #read initial extract from temporary JSON file using utilities.py
+    developer_extract = utilities.read_from_json(config['JSONarchive']['developer_extract'])
+
+    transformation_results_list = []
+        
+    for i in range(0, len(developer_extract)):
         try:
-            developers_request = requests.get(next_page).json()
-
-            for i in range(0, len(developers_request['results'])):
-                results_list.append(developers_request['results'][i])
-                game_count += developers_request['results'][i]['games_count']
-
-            logging.info(f"URL {next_page} was successful")
-
-            next_page = developers_request['next']
+            target_dictionary = {'ScrapeTimestamp': str(scrape_timestamp),
+                                'DevloperId': developer_extract[i]['id'],
+                                'DeveloperName': developer_extract[i]['name'],
+                                'DeveloperCount': developer_extract[i]['games_count']}
+            
+            transformation_results_list.append(target_dictionary)
 
         except Exception as e:
-            logging.error(f"Request failed:\n{e}")
+            logging.error(f"Transformation of ID {developer_extract[i]['id']} has failed. Exception:\n {e}")
 
+    #write intial extract to temporary JSON file using utitilities.py
+    utilities.write_to_json(transformation_results_list, config['JSONarchive']['developer_filtered'])
 
-    logging.info(f"Initial developer request was successful.\nCount of developers: {len(results_list)}\nCount of scrapeable games: {game_count}")
-        
-    
-    #write intial extract to temporary JSON file
-    utilities.write_to_json(results_list, config['JSONarchive']['developer_extract'])
-
-    
-
-
+    logging.info(f"Developer Transformation is complete.\nNumber of Records: {len(transformation_results_list)}")
 
 if __name__ == "__main__":
     scrape_timestamp = datetime.now()
-    initial_scrape(scrape_timestamp)
+    #initial_scrape()
+    transform_to_load_set(scrape_timestamp)
--- utilities.py
+++ utilities.py
@@ -1,8 +1,15 @@
 import json
 
-#function that converts dictionary/list to JSON and writes it to a file for reference/debugging
-def write_to_json(dictionary, file_path):
+#function that converts dictionary/list to JSON and writes it to a JSON document file for reference/debugging
+def write_to_json(dictionary: dict, file_path: str) -> None:
     json_output = json.dumps(dictionary)
     with open(file_path, 'w') as f:
         f.write(json_output)
         f.close()
+
+#function that allows you read a JSON document file and write it to a dictionary/list
+def read_from_json(file_path: str):
+    with open(file_path, 'r') as f:
+        dictionary = json.load(f)
+        f.close
+    return dictionary
